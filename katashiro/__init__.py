28595d53bc83b186720050687b9ee716766c5ad3
podhmo <podhmo+altair@beproud.jp>
2015-03-19 06:03:49 +0900

    prototype

--- katashiro/__init__.py
+++ katashiro/__init__.py
@@ -2,3 +2,236 @@
 import logging
 logger = logging.getLogger(__name__)
 
+
+class Conflict(Exception):
+    pass
+
+
+class Manager(object):
+    def __init__(self, seq_factory, domain_factory, atom_factory, fields_factory=list):
+        self.seq_factory = seq_factory
+        self.domain_factory = domain_factory
+        self.atom_factory = atom_factory
+        self.fields_factory = fields_factory
+
+    def compose(self, x, y):
+        x_fields, x_metadata = x.decompose()
+        y_fields, y_metadata = y.decompose()
+        self.check_fields_conflict(x_fields, y_fields, x, y)
+        x_fields.extend(y_fields)
+        metadata = {x.id: x_metadata, y.id: y_metadata}
+        return self.domain_factory(self, x.id, x_fields, metadata)
+
+    def check_fields_conflict(self, x_fields, y_fields, x, y):
+        candidates = set(f.id for f in x_fields)
+        for f in y_fields:
+            if f.id in candidates:
+                raise Conflict("{} of ({} and {})".format(f.id, x, y))
+
+    def child_id(self, x, subid):
+        return "{}.{}".format(x.id, subid)
+
+    def Domain(self, *args, **kwargs):
+        return self.domain_factory(self, *args, **kwargs)
+
+    def Atom(self, id, *args, **kwargs):
+        return self.atom_factory(self, id, *args, **kwargs)
+
+    def Seq(self, id, *args, **kwargs):
+        return self.seq_factory(self, id, *args, **kwargs)
+
+    def _include_deep(self, domain, predicate):
+        if self.is_atom(domain):
+            if predicate(domain):
+                return domain
+            else:
+                return None
+        else:
+            fields, metadata = domain.decompose()
+            new_fields = []
+            new_metadata = {}
+            for f in fields:
+                v = self._include_deep(f, predicate)
+                if v is not None:
+                    new_fields.append(v)
+                    if v.id in metadata:
+                        new_metadata[v.id] = metadata[v.id]
+            return self.Domain(domain.id, new_fields, new_metadata)
+
+    def _include_shallow(self, domain, predicate):
+        fields, metadata = domain.decompose()
+        new_fields = []
+        new_metadata = {}
+        for f in fields:
+            if predicate(f):
+                new_fields.append(f)
+            if f.id in metadata:
+                new_metadata[f.id] = metadata[f.id]
+        return self.Domain(domain.id, new_fields, new_metadata)
+
+    def include(self, domain, predicate, deep=True):
+        if deep:
+            return self._include_deep(domain, predicate)
+        else:
+            return self._include_shallow(domain, predicate)
+
+    def exclude(self, domain, predicate, deep=True):
+        return self.include(domain, lambda d: not(predicate(d)), deep=deep)
+
+    def rename(self, domain, names):
+        if self.is_atom(domain):
+            if domain.id in names:
+                return self.Atom(names[domain.id], domain.metadata.copy())
+            else:
+                return domain
+        else:
+            fields = [self.rename(f, names) for f in domain.fields]
+            metadata = domain.metadata.copy()
+            return self.Domain(names.get(domain.id, domain.id), fields, metadata)
+
+    def _add_field(self, domain, field):
+        self.domain.fields.append(field)
+
+    def _add_metadata(self, domain, k, v):
+        self.domain.metadata[k] = v
+
+    def is_seq(self, metadata):
+        return metadata and metadata.get("datatype", "seq")
+
+    def is_atom(self, child):
+        return not hasattr(child, "fields")
+
+    def is_domain(self, child):
+        return hasattr(child, "decompose")
+
+    def shortcut(self, id, attributes, metadata=None):
+        r = self.fields_factory()
+        for child in attributes:
+            if isinstance(child, (list, tuple)):
+                if len(child) < 3:
+                    if isinstance(child[1], (list, tuple)):
+                        # id, fields
+                        r.append(domain(child[0], child[1]))
+                    else:
+                        sub_id = child[0]
+                        sub_fields, sub_metadata = child[1].decompose()
+                        r.append(self.Domain(sub_id, sub_fields, sub_metadata))
+                else:
+                    # id, fields, metadata
+                    r.append(domain(child[0], child[1], child[2]))
+            else:
+                # id
+                r.append(self.Atom(child))
+        if self.is_seq(metadata):
+            return self.Seq(id, r, metadata)
+        else:
+            return self.Domain(id, r, metadata)
+
+
+class _Domain(object):
+    def __init__(self, manager, id, fields=None, metadata=None):
+        self.manager = manager
+        self.id = id
+        self.fields = fields or manager.fields_factory()
+        self.cache = None
+        self.metadata = metadata or {}
+
+    def decompose(self):
+        return self.fields[:], self.metadata.copy()
+
+    def __add__(self, other):
+        return self.manager.compose(self, other)
+
+    def include(self, predicate, deep=True):
+        return self.manager.include(self, predicate, deep=deep)
+
+    def exclude(self, predicate, deep=True):
+        return self.manager.exclude(self, predicate, deep=deep)
+
+    def cut(self, ids):
+        return self.manager.exclude(self, lambda d: d.id in ids, deep=False)
+
+    def only(self, ids):
+        return self.manager.include(self, lambda d: d.id in ids, deep=False)
+
+    def __getattr__(self, id):
+        if self.cache is None:
+            self.cache = {f.id: f for f in self.fields}
+        return self.cache[id]
+
+    def rename(self, **names):
+        return self.manager.rename(self, names)
+
+    @property
+    def declared(self):
+        s = []
+        manager = self.manager
+        for f in self.fields:
+            if hasattr(f, "declared"):
+                for subid in f.declared:
+                    s.append(manager.child_id(f, subid))
+            else:
+                s.append(f.id)
+        return s
+
+    def __repr__(self):
+        fmt = '<{} id={}, declared={!r} at {}>'
+        return fmt.format(self.__class__.__name__,
+                          self.id,
+                          self.declared,
+                          hex(id(self)))
+
+
+class _Atom(object):
+    def __init__(self, manager, id, metadata=None):
+        self.manager = manager
+        self.id = id
+        self.metadata = metadata or {}
+
+    def decompose(self):
+        return [self], self.metadata.copy()
+
+
+class _Seq(_Domain):
+    def __repr__(self):
+        fmt = '<{} id={}[], declared={!r} at {}>'
+        return fmt.format(self.__class__.__name__,
+                          self.id,
+                          self.declared,
+                          hex(id(self)))
+
+
+class Symbol:
+    str = "str"
+    int = "int"
+    datetime = "datetime"
+
+
+S = Symbol
+default_domain_manager = Manager(_Seq, _Domain, _Atom)
+Domain = default_domain_manager.Domain
+Atom = default_domain_manager.Atom
+Seq = default_domain_manager.Seq
+domain = default_domain_manager.shortcut
+
+# print(Domain("Foo") + Atom("x") + Atom("y"))
+# print(Domain("Foo", [(Domain("Left") + Atom("value")), (Domain("Right") + Atom("value"))]))
+# print(Seq("People", [Domain("Person") + Atom("name") + Atom("age")]))
+# print(Domain("School", [Atom("name"), Seq("strudents", [Domain("student") + Atom("name") + Atom("grade")])]))
+
+print(domain("foo", ["x", "y"]))
+print(domain("foo", [("left", ["value"]), ("right", ["value"])]))
+print(domain("people", [("person", ["name", "age"])], {"datatype": "seq"}))
+print(domain("school", ["name", ("students", [("student", ["name", "age"])], {"datatype": "seq"})]))
+print(domain("person", ["name", "age"]) + domain("address", ["tel", "address0", "address1"]))
+print(domain("person", ["name", "age", "birth"]))
+print(domain("person", ["name", "age", "birth"]).cut("birth"))
+
+Person = (Domain("person")
+          + Atom("name", {"datatype": S.str})
+          + Atom("age", {"datatype": S.int})
+          + Atom("birth", {"datatype": S.datetime}))
+print(Person)
+Parents = domain("parents", [("mother", Person), ("father", Person)])
+print(Parents.exclude(lambda d: d.metadata.get("datatype") == S.datetime))
+print(Parents.rename(mother="M", father="F"))
